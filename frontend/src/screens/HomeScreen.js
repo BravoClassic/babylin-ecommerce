ea49c63332b4808fda20ef2c16dd99ac884b42e4
BravoClassic <gerald.akorli@gmail.com>
2021-05-17 01:12:07 +0530

    implemented the react-redux to enable store.js to work in application

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
-import axios from 'axios';
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [products, setProducts] = useState([]);
+    const productList = useSelector((state)=> state.productList);
+    const {loading,error,products} =productList;
+    const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const { data } = await axios.get('/api/products');
-                setLoading(false);
-                setProducts(data);
-            } catch (e) {
-                setError(e.message);
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, []);
+       dispatch(listProducts());
+    }, [dispatch]);
     return (
         <div>
             {
