c159033de051b3960a1b704ddae970db306574f3
BravoClassic <gerald.akorli@gmail.com>
2021-05-16 20:07:31 +0530

    🎉update the port var to point to process.env.PORT or 5000

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const data = require('./data.js');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/api/products',(req,res) =>{
     res.json(data.products);
