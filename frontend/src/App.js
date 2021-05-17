6b5d7646ecec21c69b6ce2fbe559089eb87e24ce
BravoClassic <gerald.akorli@gmail.com>
2021-05-17 16:07:05 +0530

    changed anchor to link and added the cart items to count to be rendered in homepage. added the route to the cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+    const cart = useSelector(state => state.cart);
+    const {cartItems}=cart;
   return (
       <BrowserRouter>
     <div className="grid-container">
     <header className="row">
         <div>
-            <a href="/">Babylin Consult</a>
+            <Link to="/">Babylin Consult</Link>
         </div>
         <div>
-            <a href="/cart" title="Cart"><i className="fas fa-shopping-cart"></i></a>
-            <a href="/signin" title="Sign In"><i className="fas fa-sign-in-alt"></i></a>
+            <Link to="/cart" title="Cart"><i className="fas fa-shopping-cart"></i>
+            {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+            )}
+            </Link>
+            <Link to="/signin" title="Sign In"><i className="fas fa-sign-in-alt"></i></Link>
         </div>
     </header>
     <main>
         <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/cart/:id?" component={CartScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
        
     </main>
