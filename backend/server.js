f059b0be0f3ed9336b52193cfd53945d4e7ae09b
BravoClassic <gerald.akorli@gmail.com>
2021-12-26 21:04:43 -0700

    updated server.js to get data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
-"use strict";
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product)
-        res.send(product);
-    else
-        res.status(404).send({ message: "Product not Found!" });
-});
-app.get('/api/products', (req, res) => {
-    res.json(data.products);
-});
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+})
+
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
+app.use('/api/users', userRouter);
+app.use('/api/products',productRouter);
