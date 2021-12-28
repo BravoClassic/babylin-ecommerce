a0e4f5e36b0d94f81e74e0df913cf005f0a567d0
BravoClassic <gerald.akorli@gmail.com>
2021-12-28 16:42:06 -0700

    added env config and urlencoding

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import data from './data.js';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
+dotenv.config();
 mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true});
 
 
