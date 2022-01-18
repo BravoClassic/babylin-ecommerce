8b571622d5265871ef00f8ecbbc5c0cf0d16fead
BravoClassic <gerald.akorli@gmail.com>
2022-01-17 17:35:10 -0700

    created initial state for shipping address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,16 +2,23 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 import thunk from 'redux-thunk'
 import { cartReducer } from "./reducers/cartReducers";
+import { userRegisterReducer, userSigninReducer } from "./reducers/userReducers.js";
 
 const initialState = {
     cart: {
         cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
+        shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')):{},
+    },
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
     }
 };
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userSignin:userSigninReducer,
+    userRegister:userRegisterReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
