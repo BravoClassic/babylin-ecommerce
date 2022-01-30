cc165e3ea557ca4c19b40f6182762780687e3a49
BravoClassic <gerald.akorli@gmail.com>
2022-01-30 10:10:07 -0700

    get user details to be used in order router

--- backend/utils.js
+++ backend/utils.js
@@ -11,3 +11,22 @@ export const generateToken = (user) => {
     };
     return jwt.sign(payload, process.env.JWT_SECRECT || "BruhThisIsJustaSecretYouKnow", options);
 };
+
+export const isAuth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        const onlyToken = token.slice(7, token.length);
+        jwt.verify(onlyToken, process.env.JWT_SECRECT || "BruhThisIsJustaSecretYouKnow", (err, decode) => {
+            if (err) {
+                req.stauts(401).send({ message: 'Invalid Token' });
+            }
+            else{
+                req.user = decode;
+                next();
+            }
+        });
+    }
+    else {
+        req.status(401).send({ message: 'No Token' });
+    }
+};
